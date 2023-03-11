33bbca5adc9a23184e96bd52edd661fa089c8718
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-11 09:20:19 -0500

    [add] added about section structure

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -15,7 +15,28 @@ const Home = () => {
                 <span className="brand-right">FASHION</span>
             </h2>
             <h3>Vistiendo con elegancia</h3>
-            <Button text={'Elige tu estilo'} />
+            <Link to="/tienda">
+                <Button text={'Elige tu estilo'} />
+            </Link>
+        </div>
+    );
+    const aboutSection = () => (
+        <div class="about">
+            <h3>¿Quiénes somos?</h3>
+            <p>
+                Somos un grupo de sastres artesanos enfocados en la confección
+                de prendas formales para caballeros y niños, diseñamos nuestros
+                trajes, abrigos, chaquetas y pantalones basados en altos
+                estándares de calidad y con la garantía de las mejores telas
+                nacionales y extranjeras.
+            </p>
+            <p>
+                Contamos con la experiencia necesaria para crear un diseño
+                adecuado para la ocasión de nuestros clientes lo que nos da
+                ventaja sobre otras marcas para poder complacer las exigencias
+                de nuestros consumidores.
+            </p>
+            <button>LINK</button>
         </div>
     );
 
@@ -24,7 +45,7 @@ const Home = () => {
             {introSection()}
             {pathname === '/' && <NavBar />}
             <div className="home-container" data-testid="home">
-                other content
+                {aboutSection()}
             </div>
         </div>
     );
