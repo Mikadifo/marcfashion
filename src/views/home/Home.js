e673769342029bc1cef2e9735eaaa5ce289fd050
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-10 22:16:26 -0500

    [add] added home img and styles

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 //import Button from './../../components/button/Button';
-//import clinicImg from './../../resources/imgs/home_img.jpeg';
+import homeTemp from './../../resources/imgs/home_temp.png'; //TODO: Change when get imgs
 import './Home.css';
 
 const Home = () => {
     return (
         <div className="home-container" data-testid="home">
-            <div className="intro">
-                IMG
+            <div className="intro text-center">
+                <img src={homeTemp} alt="Brand_Model_Picture" />
                 <h2>
                     <span className="brand-left">MARC</span>
                     <span className="brand-right">FASHION</span>
