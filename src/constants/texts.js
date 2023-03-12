8b0922c6e34594d08663e4930f8c0c122794343b
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-12 14:45:47 -0400

    [add] mision and vision section

--- src/constants/texts.js
+++ src/constants/texts.js
@@ -2,3 +2,7 @@ export const aboutFirstParagraph =
     'Somos un grupo de sastres artesanos enfocados en la confección de prendas formales para caballeros y niños, diseñamos nuestros trajes, abrigos, chaquetas y pantalones basados en altos estándares de calidad y con la garantía de las mejores telas nacionales y extranjeras.';
 export const aboutSecondParagraph =
     'Contamos con la experiencia necesaria para crear un diseño adecuado para la ocasión de nuestros clientes lo que nos da ventaja sobre otras marcas para poder complacer las exigencias de nuestros consumidores.';
+export const mision =
+    'Garantizar la durabilidad de la variedad de nuestros diseños exclusivos a todos nuestros clientes además de brindar una confortabilidad y experiencia única a la altura de nuestros clientes y sus magnos eventos que solo MARCFASHION puede ofrecer.';
+export const vision =
+    'Posicionarnos como una de las marcas pioneras en diseños auténticos formales para Hombres y niños utilizando la gran variedad de finos casimires que se ofertan en el extranjero y que serán importados por MARCFASHION.';
--- src/views/about/About.js
+++ src/views/about/About.js
@@ -1,4 +1,30 @@
+import FooterHero from '../../components/footerHero/FooterHero';
+import { mision, vision } from './../../constants/texts';
+import './About.css';
+import './HistorySection.css';
+import './MisionVisionSection.css';
+
 const About = () => {
-    return <>About</>;
+    const historySection = () => <div className="history">HISTORY</div>;
+    const misionVisionSection = () => (
+        <div className="mision-vision row text-center text-sm-start">
+            <div className="col-12 col-md-6 ps-0">
+                <h3>Misión</h3>
+                <p className="text-start">{mision}</p>
+            </div>
+            <div className="col-12 col-md-6 pe-0">
+                <h3>Visión</h3>
+                <p className="text-start">{vision}</p>
+            </div>
+        </div>
+    );
+
+    return (
+        <div data-testid="about">
+            {historySection()}
+            {misionVisionSection()}
+            <FooterHero background={'white'} />
+        </div>
+    );
 };
 export default About;
