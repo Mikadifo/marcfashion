c13502fbef8d15273e9426e72cf4ec68e64dfd41
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-12 18:02:18 -0400

    [mod] added buttons functionlaity

--- src/views/contact/Contact.js
+++ src/views/contact/Contact.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import Button from '../../components/button/Button';
 import FooterHero from '../../components/footerHero/FooterHero';
 import './Contact.css';
@@ -9,11 +8,11 @@ const Contact = () => {
             <h3>Ubicación</h3>
             <div className="row">
                 <div className="col">MAP</div>
-                <div className="col">
+                <div className="col text-start">
                     <h5>Puedes visitarnos físicamente</h5>
-                    <Link>
+                    <a href="#" target="_blank" rel="noreferrer">
                         <p>San Pedro, Quisapincha, Ambato - Ecuador.</p>
-                    </Link>
+                    </a>
                 </div>
             </div>
         </div>
@@ -26,26 +25,34 @@ const Contact = () => {
                     Llámanos o escríbenos por Whatsapp
                 </h5>
                 <div className="col-12 col-sm-6 ps-0 pe-0 pe-sm-4 pb-4 pb-sm-0">
-                    <Link>
+                    <a
+                        href="https://wa.me/593995487770"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         {/*TODO: extract this*/}
                         <Button text={'+ 593 99 548 7770'} type="button" />
-                    </Link>
+                    </a>
                 </div>
                 <div className="col-12 col-sm-6 p-0">
-                    <Link>
+                    <a
+                        href="https://wa.me/593996844966"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
                         <Button text={'+ 593 99 684 4966'} type="button" />
-                    </Link>
+                    </a>
                 </div>
             </div>
             <div className="row text-center text-sm-start">
                 <h5 className="pe-0 ps-0">Envianos un email</h5>
                 <div className="col p-0">
-                    <Link>
+                    <a href="mailto:alexistoala@outlook.com">
                         <Button
                             text={'alexistoala@outlook.com'}
                             type="button"
                         />
-                    </Link>
+                    </a>
                 </div>
             </div>
         </div>
