3c742ad3af51dfbfb67193209c0e61f7aced3ea4
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-13 11:06:31 -0400

    [mod] changed search bar display on small screends

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -6,19 +6,21 @@ const Search = () => {
         <div className="search text-center">
             <h3>Elige tu estilo</h3>
             <form className="row search-bar">
-                <div className="col-10">
+                <div className="col-12 col-md-10">
                     <input
                         type="search"
                         placeholder="Buscar"
                         aria-label="Search"
                     />
                 </div>
-                <div className="col-2">
+                <div className="col-2 d-none d-md-flex">
                     <Button text={'Buscar'} type="button" />
                 </div>
             </form>
             <div className="text-start d-flex search-caption">
-                <p className="my-auto">No encuentras lo que necesitas?</p>
+                <p className="my-auto d-none d-sm-flex">
+                    No encuentras lo que necesitas?
+                </p>
                 <Button text={'Personaliza tu prenda'} type="link" />
             </div>
         </div>
