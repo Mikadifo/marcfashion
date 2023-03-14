4708e65220835cc5560cffe4c8df1ad51b59b76e
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-13 21:12:05 -0400

    [mod] store added filter items structure

--- src/views/store/Store.js
+++ src/views/store/Store.js
@@ -5,10 +5,10 @@ const Store = () => {
     return (
         <div className="store" data-testid="store">
             <Search />
-            {/*
-	      TODO: top padding goes on the next div
-	    */}
-            STORE
+            <div className="row items-container">
+                <div className="col">FILTERS</div>
+                <div className="col">ITEMS</div>
+            </div>
         </div>
     );
 };
