296812976e6dcf3df48032711783de8790265114
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-14 12:55:48 -0400

    [mod] added more temporal items

--- src/resources/info/items.js
+++ src/resources/info/items.js
@@ -36,4 +36,22 @@ export const items = [
         name: 'Example Item Temp',
         price: '$50.99',
     },
+    {
+        id: 'example_item_7',
+        img: 'https://images.unsplash.com/photo-1598808503746-f34c53b9323e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        name: 'Example Item Temp',
+        price: '$50.99',
+    },
+    {
+        id: 'example_item_8',
+        img: 'https://images.unsplash.com/photo-1598808503746-f34c53b9323e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        name: 'Example Item Temp',
+        price: '$50.99',
+    },
+    {
+        id: 'example_item_9',
+        img: 'https://images.unsplash.com/photo-1598808503746-f34c53b9323e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        name: 'Example Item Temp',
+        price: '$50.99',
+    },
 ];
