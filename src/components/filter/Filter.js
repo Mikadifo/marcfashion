4711723f87014e3001be804bf55835e42caf11b4
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-03-13 22:06:57 -0400

    [mod] added filter list to compoennt

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -1,4 +1,5 @@
 import Button from '../button/Button';
+import { filters } from './../../resources/info/filters';
 import './Filter.css';
 
 const Filter = () => {
@@ -7,7 +8,33 @@ const Filter = () => {
             <div className="filter-button">
                 <Button text={'<< Ocultar filtros'} type="link" />
             </div>
-            <div className="filters-container">BOXBG</div>
+            <div className="filters-container">
+                {filters.map((filter) => (
+                    <div key={filter.id}>
+                        <h6>{filter.title}</h6>
+                        {filter.options.map((option) => (
+                            <div key={option.id}>
+                                {option.type === 'checkbox' ? (
+                                    <div>
+                                        <input
+                                            type="checkbox"
+                                            id={option.id}
+                                            name={option.id}
+                                            value={option.value}
+                                        />
+                                        <label htmlFor={option.id}>
+                                            {option.value}
+                                        </label>
+                                    </div>
+                                ) : (
+                                    'COLORS HERE'
+                                )}
+                                {/*TODO: add a custom check color component*/}
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
