b3c53d9c56baf09e267db825694fd5b2096fb688
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-04 14:20:37 -0400

    [add] added pana jackets imgs

---
+++ src/resources/info/items/pana_jackets_imgs.js
@@ -0,0 +1,52 @@
+//negro -> color_1
+import chaqueta_pana_negro1 from './../../imgs/pana_jackets/chaqueta_pana_negro1.jpeg';
+//azul marino -> color_2
+import chaqueta_pana_azul_marino1 from './../../imgs/pana_jackets/chaqueta_pana_azul_marino1.jpeg';
+//gris -> color_3
+import chaqueta_pana_gris1 from './../../imgs/pana_jackets/chaqueta_pana_gris1.jpeg';
+//plomo -> color_4
+import chaqueta_pana_plomo1 from './../../imgs/pana_jackets/chaqueta_pana_plomo1.jpeg';
+//azul electrico -> color_5
+import chaqueta_pana_azul_electrico1 from './../../imgs/pana_jackets/chaqueta_pana_azul_electrico1.jpeg';
+//vino -> color_6
+import chaqueta_pana_vino1 from './../../imgs/pana_jackets/chaqueta_pana_vino1.jpeg';
+//habano -> color_7
+import chaqueta_pana_habano_1 from './../../imgs/pana_jackets/chaqueta_pana_habano_1.jpeg';
+
+export const panaJacketsImgs = [
+    {
+        id: 'img_chaqueta_pana_negro_1',
+        url: chaqueta_pana_negro1,
+        color: 'color_1',
+    },
+    {
+        id: 'img_chaqueta_pana_azul_marino_1',
+        url: chaqueta_pana_azul_marino1,
+        color: 'color_2',
+    },
+    {
+        id: 'img_chaqueta_pana_gris_1',
+        url: chaqueta_pana_gris1,
+        color: 'color_3',
+    },
+    {
+        id: 'img_chaqueta_pana_plomo_1',
+        url: chaqueta_pana_plomo1,
+        color: 'color_4',
+    },
+    {
+        id: 'img_chaqueta_pana_azul_electrico_1',
+        url: chaqueta_pana_azul_electrico1,
+        color: 'color_5',
+    },
+    {
+        id: 'img_chaqueta_pana_vino_1',
+        url: chaqueta_pana_vino1,
+        color: 'color_6',
+    },
+    {
+        id: 'img_chaqueta_pana_habano_1',
+        url: chaqueta_pana_habano_1,
+        color: 'color_7',
+    },
+];
