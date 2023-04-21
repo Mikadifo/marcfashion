c8e307e5e79f34d7bfc6077a479d068af1fe15c2
Mikadifo <mikadifopadillaheredia@gmail.com>
2023-04-21 09:14:24 -0400

    [add] added new squared jackets imgs #13

--- src/resources/info/items/square_jackets_imgs.js
+++ src/resources/info/items/square_jackets_imgs.js
@@ -1,44 +1,89 @@
 //negro -> color_1
-//TODO: Missing this color
-import noColorLinked from './../../imgs/noColorLinked.png';
+import chaqueta_cuadros_negro1 from './../../imgs/squared_jackets/chaqueta_cuadros_negro1.jpeg';
+import chaqueta_cuadros_negro2 from './../../imgs/squared_jackets/chaqueta_cuadros_negro2.jpeg';
 //azul marino -> color_2
-//TODO: Missing this color
+import chaqueta_cuadros_azul_marino1 from './../../imgs/squared_jackets/chaqueta_cuadros_azul_marino1.jpeg';
+import chaqueta_cuadros_azul_marino2 from './../../imgs/squared_jackets/chaqueta_cuadros_azul_marino2.jpeg';
 //gris -> color_3
-//TODO: Missing this color
+import chaqueta_cuadros_gris1 from './../../imgs/squared_jackets/chaqueta_cuadros_gris1.jpeg';
+import chaqueta_cuadros_gris2 from './../../imgs/squared_jackets/chaqueta_cuadros_gris2.jpeg';
+import chaqueta_cuadros_gris3 from './../../imgs/squared_jackets/chaqueta_cuadros_gris3.jpeg';
 //plomo -> color_4
-//TODO: Missing this color
+import chaqueta_cuadros_plomo1 from './../../imgs/squared_jackets/chaqueta_cuadros_plomo1.jpeg';
+import chaqueta_cuadros_plomo2 from './../../imgs/squared_jackets/chaqueta_cuadros_plomo2.jpeg';
+import chaqueta_cuadros_plomo3 from './../../imgs/squared_jackets/chaqueta_cuadros_plomo3.jpeg';
 //azul electrico -> color_5
-//TODO: Missing this color
+import chaqueta_cuadros_azul_electrico1 from './../../imgs/squared_jackets/chaqueta_cuadros_azul_electrico1.jpeg';
+import chaqueta_cuadros_azul_electrico2 from './../../imgs/squared_jackets/chaqueta_cuadros_azul_electrico2.jpeg';
+import chaqueta_cuadros_azul_electrico3 from './../../imgs/squared_jackets/chaqueta_cuadros_azul_electrico3.jpeg';
 //vino -> color_6
 //TODO: Missing this color
 //habano -> color_7
 //TODO: Missing this color
+//celeste -> color_undefined
+import chaqueta_cuadros_celeste1 from './../../imgs/squared_jackets/chaqueta_cuadros_celeste1.jpeg';
+import chaqueta_cuadros_celeste2 from './../../imgs/squared_jackets/chaqueta_cuadros_celeste2.jpeg';
+import chaqueta_cuadros_celeste3 from './../../imgs/squared_jackets/chaqueta_cuadros_celeste3.jpeg';
+
+import noColorLinked from './../../imgs/noColorLinked.png';
 
 export const squareJacketsImgs = [
     {
         id: 'img_sq_jacket_negro_1',
-        url: noColorLinked,
+        url: chaqueta_cuadros_negro1,
         color: 'color_1',
     },
     {
+        id: 'img_sq_jacket_negro_2',
+        url: chaqueta_cuadros_negro2,
+    },
+    {
         id: 'img_sq_jacket_azul_marino_1',
-        url: noColorLinked,
+        url: chaqueta_cuadros_azul_marino1,
         color: 'color_2',
     },
     {
+        id: 'img_sq_jacket_azul_marino_2',
+        url: chaqueta_cuadros_azul_marino2,
+    },
+    {
         id: 'img_sq_jacket_gris_1',
-        url: noColorLinked,
+        url: chaqueta_cuadros_gris1,
         color: 'color_3',
     },
     {
+        id: 'img_sq_jacket_gris_2',
+        url: chaqueta_cuadros_gris2,
+    },
+    {
+        id: 'img_sq_jacket_gris_3',
+        url: chaqueta_cuadros_gris3,
+    },
+    {
         id: 'img_sq_jacket_plomo_1',
-        url: noColorLinked,
+        url: chaqueta_cuadros_plomo1,
         color: 'color_4',
     },
     {
+        id: 'img_sq_jacket_plomo_2',
+        url: chaqueta_cuadros_plomo2,
+    },
+    {
+        id: 'img_sq_jacket_plomo_3',
+        url: chaqueta_cuadros_plomo3,
+    },
+    {
         id: 'img_sq_jacket_azul_electrico_1',
-        url: noColorLinked,
+        url: chaqueta_cuadros_azul_electrico1,
         color: 'color_5',
+    },
+    {
+        id: 'img_sq_jacket_azul_electrico_2',
+        url: chaqueta_cuadros_azul_electrico2,
+    },
+    {
+        id: 'img_sq_jacket_azul_electrico_3',
+        url: chaqueta_cuadros_azul_electrico3,
     },
     {
         id: 'img_sq_jacket_vino_1',
@@ -49,5 +94,17 @@ export const squareJacketsImgs = [
         id: 'img_sq_jacket_habano_1',
         url: noColorLinked,
         color: 'color_7',
+    },
+    {
+        id: 'img_sq_jacket_celeste_1',
+        url: chaqueta_cuadros_celeste1,
+    },
+    {
+        id: 'img_sq_jacket_celeste_2',
+        url: chaqueta_cuadros_celeste2,
+    },
+    {
+        id: 'img_sq_jacket_celeste_3',
+        url: chaqueta_cuadros_celeste3,
     },
 ];
